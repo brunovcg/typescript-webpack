e8468115a7c76bd265cb171276f4081cd2049fe0
Bruno Gouveia <brunovcg@gmail.com>
2022-03-12 11:05:33 -0300

    husky

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import GOOGLE from './google.svg'
 import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-    const a = 2
+  const a = 33
 
-    return (
-        <>
-            <img src={IMAGE} alt="react-logo" />
-            <img src={GOOGLE} alt="google-logo" />
-            <div>React Typescript Webpack Starter</div>
-            <div>
-                Modo: {process.env.NODE_ENV} Variavel: {process.env.name}
-                {a}
-            </div>
-            <ClickCounter />
-        </>
-    )
+  return (
+    <>
+      <img src={IMAGE} alt="react-logo" />
+      <img src={GOOGLE} alt="google-logo" />
+      <div>React Typescript Webpack Starter</div>
+      <div>
+        Modo: {process.env.NODE_ENV} Variavel: {process.env.name}
+        {a}
+      </div>
+      <ClickCounter />
+    </>
+  )
 }
