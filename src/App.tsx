0ede3a7fe0502a879736560a77dadcef570ea981
Bruno Gouveia <brunovcg@gmail.com>
2022-03-12 10:57:32 -0300

    husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
-export const App = () => {
+import './styles.css'
+import IMAGE from './react.png'
+import GOOGLE from './google.svg'
+import { ClickCounter } from './ClickCounter'
 
-    return <div>React Typescript Webpack Starter </div>
+export const App = () => {
+ return (
+  <>
+   <img src={IMAGE} alt="react-logo" />
+   <img src={GOOGLE} alt="google-logo" />
+   <div>React Typescript Webpack Starter</div>
+   <div>
+    Modo: {process.env.NODE_ENV} Variavel: {process.env.name}
+   </div>
+   <ClickCounter />
+  </>
+ )
 }
