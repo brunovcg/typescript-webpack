6bba6647f9f99a25c05da6c3718e4099572bcb5a
Bruno Gouveia <brunovcg@gmail.com>
2022-03-12 11:42:18 -0300

    final

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,13 +1,13 @@
-const {merge} = require("webpack-merge")
+const { merge } = require('webpack-merge')
 
 const commonConfig = require('./webpack.common')
 
 module.exports = (envVars) => {
-    const {env} = envVars
+  const { env } = envVars
 
-    const envConfig = require(`./webpack.${env}.js`)
+  const envConfig = require(`./webpack.${env}.js`)
 
-    const config = merge(commonConfig, envConfig)
+  const config = merge(commonConfig, envConfig)
 
-    return config
+  return config
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,16 +2,16 @@ const webpack = require('webpack')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    devServer: {
-        hot: true,
-        // port: 3000
-    },
-    devtool: 'cheap-module-source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.name': JSON.stringify('Bruno'),
-        }),
-        new ReactRefreshWebpackPlugin(),
-    ],
+  mode: 'development',
+  devServer: {
+    hot: true,
+    // port: 3000
+  },
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Bruno'),
+    }),
+    new ReactRefreshWebpackPlugin(),
+  ],
 }
